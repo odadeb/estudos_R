95fbe5dbc4934fc9eb5d1cdde04842ee4d4011ba
Odair Junior <odair.junior.rodrigues@gmail.com>
2020-08-04 17:39:26 -0300

    Add files via upload

---
+++ Estudos em R/#estudo_5/estudo.r
@@ -0,0 +1,6 @@
+num <- c(1,2,3,4,6,5,4,77,8,12,23,34,35,52,34,54,54,54,12,21,34,16,38)
+# variancia <- var(num)
+# print(variancia)
+# sqrt(variancia)
+desvio_padrao <- sd(num)
+print(desvio_padrao)
---
+++ Estudos em R/#estudo_5/exec-csv.r
@@ -0,0 +1,4 @@
+read <- read.csv(file="/home/odair/Documentos.Analytics/Estudos em R/#estudo_5/file_csv.csv")
+print(read)
+hist(read$X2)
+# $X1 indica a primeira coluna do documento
