ec316fcb61e30b9ea73fec875bbb7ec358e4af07
Odair Junior <odair.junior.rodrigues@gmail.com>
2020-08-04 17:02:29 -0300

    Add files via upload

---
+++ Estudos em R/#estudo_1/estudo.r
@@ -0,0 +1,16 @@
+#AULA 02 Introdução a estatistica com R
+
+# numero <- 1+1
+# # Foi definido a variavél numero onde o simbulo para indicar 
+# #o que tem dentro é "<-" que é uma seta
+# print(numero)
+# #Utiliza_se o comando "print" para mostrar o resultado
+# #resultado esperado: 2
+lista <- c(1,2,3,4,5,6,7,8)
+#o comanda "c" é usado para montar uma lista
+print(lista)
+#resultado esperado [1] 1 2 3 4 5 6 7 8
+
+#MULTIPLICAÇÃO DE UMA LISTA
+multi_lista <- lista * 2
+print(multi_lista)
---
+++ Estudos em R/#estudo_1/estudo_r_1.r
@@ -0,0 +1,9 @@
+# Exemplo de como utilizar o R
+
+# NESSE EXEMPLO TEMOS UMA ESCOLA ONDE OS COLOCARAM 
+# EM UMA PESQUISA MENSAL QUANTAS HORAS DE MATERIAS COMPLEMENTARES O ALUNO ESTUDOU
+
+aulas <- c(2,4,4,4,6,6,6,6,5,5,5,5,4,3,2,1,1,1,1,8,7)
+# print(aulas)
+hist(aulas)
+#resultado esperado 'img_01' na pasta imagens de resultado
