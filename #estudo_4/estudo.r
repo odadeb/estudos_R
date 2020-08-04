7caf1c1b3e8519da878e4fe8550470e90bd224c1
Odair Junior <odair.junior.rodrigues@gmail.com>
2020-08-04 17:42:21 -0300

    Delete estudo.r

--- #estudo_4/estudo.r
+++
@@ -1,8 +0,0 @@
-num <- c(1,1,1,3,3,4,4,4,5,6,7,12,13,15,18,30,55,56,58,23,24,1,4,5,6)
-# print(num)
-ptnum <- (summary(num))
-# print(ptnum)
-png(file="/home/odair/Documentos.Analytics/Estudos em R/imagens/img_2.png", width=700, height=700)
-boxplot(num)
-dev.off
-
